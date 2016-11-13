1829f7a27830653299873155c337bf2fe13e2bab
Markus Mattsson <markus.mattsson@gmail.com>
2016-11-14 00:56:08 +0100

    Add infinite scrolling

--- src/Search.js
+++ src/Search.js
@@ -2,60 +2,106 @@ import React, { Component } from 'react';
 import '../lib/500px.js';
 import SearchResult from './SearchResult';
 
+/**
+ *
+ */
 class Search extends Component {
 
     constructor(props) {
         super(props);
 
-        window._500px.init({
-            sdk_key: '26aa2230f2ef55c070ef4efe62fab4c97fc305be'
-        });
-        console.log("Search#constructor - initialData: ", this.props.initialData);
+        if (!window._500px.sdk_key) {
+            window._500px.init({
+                sdk_key: '26aa2230f2ef55c070ef4efe62fab4c97fc305be'
+            });
+        }
 
         this.state = {
+            term: this.props.params.term ? this.props.params.term : "",
             inProgress: false,
-            data: this.props.initialData ? this.props.initialData : {}
+            data: {}
         };
 
+        this.containsSameId = this.containsSameId.bind(this);
+        this.filterOutDuplicates = this.filterOutDuplicates.bind(this);
+
+    }
+
+    componentWillMount() {
+        this.runSearch(this.props.params.term, 1);
     }
 
     componentWillReceiveProps(props) {
-        var thus = this;
-
         var term = props.params.term;
-        console.log("Search#componentWillReceiveProps - term: ", term);
+        if (!this.state.inProgress) {
+            this.runSearch(term, 1);
+        }
+    }
+
+    containsSameId(oldPhotos, id) {
+        for (var i = 0; i < oldPhotos.length; i++) {
+            if (id === oldPhotos[i].id) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    filterOutDuplicates(oldPhotos, newPhotos) {
+        return newPhotos.filter(function(image) {
+            return !this.containsSameId(oldPhotos, image.id);
+        }, this);
+    }
+
+    runSearch(term, page) {
         if (term) {
+            var data = this.state.data;
+            if (this.state.term !== term) {
+                data = {};
+            }
             this.setState({
-                inProgress: true
+                term: term,
+                inProgress: true,
+                data: data
             });
             var options = {
                 term: term,
-                image_size: 200
+                page: page,
+                image_size: [6, 200]
             };
-            window._500px.api("/photos/search", options, function(response) {
-                var data = {};
+            var thus = this;
+            window._500px.api("/photos/search", options, function (response) {
+                var newData = thus.state.data;
+
                 if (response.success) {
-                    data = response.data;
+                    if (newData.photos) {
+                        newData['current_page'] = response.data['current_page'];
+                        newData.photos.push.apply(newData.photos, thus.filterOutDuplicates(newData.photos, response.data.photos));
+                    } else {
+                        newData = response.data;
+                    }
                 }
                 thus.setState({
                     inProgress: false,
-                    data: data
+                    data: newData
                 });
             });
         }
+    }
 
+    loadNextPage() {
+        if (this.state.data['current_page'] < this.state.data['total_pages']) {
+            this.runSearch(this.state.term, this.state.data['current_page'] + 1);
+        }
     }
 
     render() {
-        if (this.state.inProgress) {
-            return (
-                <div>Search is in progress...</div>
-            )
-        } else {
-            return (
-                <SearchResult data={this.state.data} />
-            )
-        }
+        return (
+            <SearchResult
+                data={this.state.data}
+                searchInProgress={this.state.inProgress}
+                onScrollToEnd={this.loadNextPage.bind(this)}/>
+        );
     }
 }
 
--- src/SearchResult.js
+++ src/SearchResult.js
@@ -1,25 +1,93 @@
 import React, { Component } from 'react';
-import ResultItem from './ResultItem';
+import Waypoint from 'react-waypoint';
 
+/**
+ * Displays search results in the form of a responsive, infinitely scrolling grid.
+ */
 class SearchResult extends Component {
-    render() {
-        console.log("SearchResult#render()");
-        var items = [];
 
-        if (this.props.data.photos && this.props.data.photos.length > 0) {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true
+        }
+    }
 
-            // TODO: If last item is rendered, call back to add next page etc..
-            this.props.data.photos.forEach(function (image) {
-                items.push(<ResultItem key={image.id} image={image}/>);
+    componentWillReceiveProps(props) {
+        this.setState({
+            isLoading: props.searchInProgress
+        })
+    }
+
+    allImagesLoaded(container) {
+        const allImages = container.querySelectorAll('img');
+        for (const image of allImages) {
+            if (!image.complete) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    imageLoaded() {
+        if (this.state.isLoading && this.allImagesLoaded(this.refs['search-result-items'])) {
+            this.setState({
+                isLoading: false
             });
+        }
+    }
+
+    getImageUrl(image, size) {
+        return image.images.filter(function (o) {
+            return o.size === size
+        })[0].url;
+    }
+
+    renderImage(image) {
+        return (
+            <div key={image.id} className="pure-u-1 pure-u-sm-1-2 pure-u-md-1-3 pure-u-lg-1-4 pure-u-xl-1-5">
+                <div className="result-item">
+                    <a target="_blank" href={this.getImageUrl(image, 6)}>
+                        <img
+                            onLoad={this.props.imageLoaded}
+                            src={this.getImageUrl(image, 200)}
+                            alt={image.name}/>
+                        <p>© {image.user.fullname} / 500px</p>
+                    </a>
+                </div>
+            </div>
+        )
+    }
+
+    renderWaypoint() {
+        if (!this.state.isLoading && this.props.data['current_page'] < this.props.data['total_pages']) {
+            return (
+                <div>
+                    <Waypoint
+                        scrollableAncestor={window}
+                        onEnter={this.props.onScrollToEnd}/>
+                    <p>Loading more items...</p>
+                </div>
+            );
+        }
+    }
+
+    render() {
+        if (this.props.data.photos && this.props.data.photos.length > 0) {
 
             return (
                 <div className="search-result">
-                    <div className="pure-g">
-                        {items}
+                    <div className="pure-g" ref="search-result-items">
+                        {this.props.data.photos.map(image => this.renderImage(image))}
                     </div>
+                    {this.renderWaypoint()}
                 </div>
             );
+
+        } else if (this.props.searchInProgress) {
+            return (
+                <div>Searching...</div>
+            )
         } else {
             return (
                 <div>Search rendered no results...</div>
@@ -27,5 +95,22 @@ class SearchResult extends Component {
         }
     }
 }
+SearchResult.propTypes = {
+    /**
+     * Indicates whether a search is currently in progress or not.
+     */
+    searchInProgress: React.PropTypes.bool.isRequired,
+
+    /**
+     * The result data that is to be displayed.
+     */
+    data: React.PropTypes.object.isRequired,
+
+    /**
+     * Callback to let parent component know when the user has scrolled
+     * to the end and is expecting the next batch of data.
+     */
+    onScrollToEnd: React.PropTypes.func.isRequired
+};
 
 export default SearchResult;
